68c12fe4aee4bdada17c3641a3bd7bc2b6487ab6
safakilic5896 <lhystrikk@gmail.com>
2021-03-01 21:36:34 +0100

    feat(001): Initialiser le projet Java Spring boot

---
+++ src/main/java/fr/epita/pfa/terroirback/TerroirBackApplication.java
@@ -0,0 +1,15 @@
+package fr.epita.pfa.terroirback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@SpringBootApplication
+@EnableSwagger2
+public class TerroirBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TerroirBackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fr/epita/pfa/terroirback/TerroirBackApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.epita.pfa.terroirback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TerroirBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
